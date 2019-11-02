a1776035562b172582ff731fe2df510ee9fcde41
saif43 <32043353+saif43@users.noreply.github.com>
2019-11-02 15:28:53 +0600

    Email fix

--- php/mail.php
+++ php/mail.php
@@ -2,12 +2,20 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$from = 'info@theicthub.com';
+$from = 'From: DGNM <info@theicthub.com>';
 $to = 'saif.ahmed.anik.0@gmail.com';
 
 $subject = 'PHP Mail Sending Checking';
 $msg = 'PHP Mail Works fine';
-$header =  'From: '.$from;
+
+$header = "Reply-To: The Sender <info@theicthub.com>\r\n";
+$header .= "Return-Path: The Sender <info@theicthub.com>\r\n";
+$header .= "From: The Sender <info@theicthub.com>\r\n";
+$header .= "Organization: UNFPA\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$header .= "X-Priority: 3\r\n";
+$header .= "X-Mailer: PHP". phpversion() ."\r\n";
 
 mail($to,$subject,$msg,$header);
 
