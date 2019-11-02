8b63c4dcb011fffd9d19c835cfb8e44919049996
saif43 <32043353+saif43@users.noreply.github.com>
2019-11-02 13:11:16 +0600

    keep mail.php only

--- php/mail.php
+++ php/mail.php
@@ -1,45 +1,16 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-//Load composer's autoloader
-require 'vendor/autoload.php';
+$from = 'info@theicthub.com';
+$to = 'saif.ahmed.anik.0@gmail.com';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'sanik141149@bscse.uiu.ac.bd';                 // SMTP username
-    $mail->Password = '102030405060708090100';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+$subject = 'PHP Mail Sending Checking';
+$msg = 'PHP Mail Works fine';
+$header =  'From: '.$from;
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Saif Ahmed Anik');
-    $mail->addAddress('saif.ahmed.anik.0@gmail.com', 'Saif Ahmed Anik');     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+mail($to,$subject,$msg,$header);
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+echo 'msg was sent successfully';
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-}
+?>
