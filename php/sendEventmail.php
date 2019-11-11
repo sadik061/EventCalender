3b8e1455e15a20a5ce3b0102a8abbabb46100706
saif43 <saif.ahmed.anik.0@gmail.com>
2019-11-11 21:38:14 +0600

    email module added

---
+++ php/sendEventmail.php
@@ -0,0 +1,43 @@
+<?php
+include 'database.php';
+include 'mail.php';
+
+$subject = 'DGNM Event Remainder';
+$msg = '';
+
+$query = "SELECT * FROM events NATURAL JOIN participents NATURAL JOIN instructor WHERE events.start_event LIKE ";
+$query .= "CONCAT(date(CURRENT_DATE+1),'%')";
+
+
+$statement = $connect->prepare($query);
+$statement->execute();
+// echo $statement->rowCount();
+
+if ($statement->rowCount() > 0) {
+    $result = $statement->fetchAll();
+    $n = $statement->rowCount();
+
+    for ($i = 0; $i < $n; $i++) {
+        $msg .=  "Dear " . $result[$i]['name'] . ",";
+        $msg .= "\r\n";
+
+        $msg .= "You have a event tomorrow.";
+        $msg .= "\r\n";
+
+        $msg .= "Event Information:";
+        $msg .= "\r\n";
+
+        $msg .= "Event name: " . $result[$i]['title'];
+        $msg .= "\r\n";
+
+        $msg .= "Time: " . $result[$i]['start_event'] . " to " . $result[$i]['end_event'];
+        $msg .= "\r\n";
+
+        include 'mail.php';
+
+        sendEmail($email,$subject,$msg);
+
+    }
+}
+
+?>
