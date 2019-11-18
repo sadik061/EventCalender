572c836ddd945ce60e84580fe29cc98d0d067be9
arefin070 <arefinislam070@gmail.com>
2019-11-18 18:20:12 +0600

    user guide

---
+++ userguide.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel='icon' href='img/dgnm.png' type='image/x-icon'/ >
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>DGNM</title>
+</head>
+<body data-gr-c-s-loaded="true">
+
+<body id="page-top">
+
+  <!-- Navigation -->
+ 
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>Welcome to the user guide</h1>
+      <p class="lead">A user guide for DGNM's "Event Calendar Application" </p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Calendar</h2>
+          <p class="lead">This is the <b>Dashboard</b> of this Application. Here we have included every steps of creating events, customizing, shifting and multiple views of event on calendar views.</p>
+          <div class="container">
+            <div id="accordion">
+              <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                     Calendar Views
+                    </button>
+                  </h5>
+                </div>
+            
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                  <div class="card-body">
+                    <div class="row">
+                      <img src="img\Dashboard.Calendar.png" class="img-fluid" alt="Responsive image">
+                      <p style="padding-top: 2%"><b>1. Calendar view:</b> Without any events.</p>
+                    </div>
+                    <div class="row">
+                      <img src="img\cal.create.png" class="img-fluid" alt="Responsive image">
+                      <p style="padding-top: 2%"><b>2. Create Events:</b> Create events from Dashboard.</p>
+                    </div>
+                    <div class="row">
+                        <img src="img\cal.event.png" class="img-fluid" alt="Responsive image">
+                        <p style="padding-top: 2%"><b>3. Insert Events Information:</b> Insert event Information including - event name, date, venu, funded by, etc. For next attempt, Please click <i><b>"Choose Participant"</b></i> button to add Participant (Instructors/Resource person). </p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header" id="headingTwo">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Choose Participent
+                    </button>
+                  </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                  <div class="card-body">
+                      <div class="row">
+                          <img src="img\cal.participents.png" class="img-fluid" alt="Responsive image">
+                          <p style="padding-top: 2%"><b>1. Search Institute:</b> Search any <i><b>"institutes"</b></i> from database. You can even search by area. </p>
+                        </div>
+                        <div class="row">
+                            <img src="img\cal.instructors2.png" class="img-fluid" alt="Responsive image">
+                            <p style="padding-top: 2%"><b>2. Add Participants:</b> Add any <i><b>"Participant"</b></i> from choosen Institute. You can choose participants as <i><b>"Role" </b></i>as given under the Assign button. </p>
+                          </div>
+                        <div class="row">
+                            <img src="img\cal.instructors3.png" class="img-fluid" alt="Responsive image">
+                            <p style="padding-top: 2%"><b>3. Participants Tabs:</b> Here are two participant tabs, first one is <i><b>"Participant Tab"</b></i> and second one is <i><b>"Resource Person Tab"</b></i>. You can add new <b>Resource Person </b>from <i><b>"Resource Person Tab"</b></i>. </p>
+                        </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Events</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+          <div class="container">
+              <div class="card">
+                  <div class="card-header" id="headingThree">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Event List
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="row">
+                            <img src="img\cal.eventsList.png" class="img-fluid" alt="Responsive image">
+                            <p style="padding-top: 2%"><b>1. Search any Events:</b> Here you can search any events by its name. You also can add new <b>Events</b> by clicking <i><b>"Add new Event"</b></i> button from top-left corner of the page.</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header" id="headingThree">
+                      <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          Add New Events
+                        </button>
+                      </h5>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                      <div class="card-body">
+                          <div class="row">
+                              <img src="img\event.addnew.png" class="img-fluid" alt="Responsive image">
+                              <p style="padding-top: 2%"><b>1. Add New Events:</b> Create new event from <b>"Event"</b> page.</p>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+
+                
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Contact us</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+          <div class="container">
+              <div class="card">
+                  <div class="card-header" id="headingThree">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Event List
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="row">
+                            <img src="img\cal.eventsList.png" class="img-fluid" alt="Responsive image">
+                            <p style="padding-top: 2%"><b>1. Search any Events:</b> Here you can search any events by its name. You also can add new <b>Events</b> by clicking <i><b>"Add new Event"</b></i> button from top-left corner of the page.</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header" id="headingThree">
+                      <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          Add New Events
+                        </button>
+                      </h5>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                      <div class="card-body">
+                          <div class="row">
+                              <img src="img\event.addnew.png" class="img-fluid" alt="Responsive image">
+                              <p style="padding-top: 2%"><b>1. Add New Events:</b> Create new event from <b>"Event"</b> page.</p>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+
+                
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Contact us</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Contact us</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
