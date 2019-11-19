58a29330dfd58a53603e044ec3d7390de422be81
arefin070 <arefinislam070@gmail.com>
2019-11-19 13:34:25 +0600

    user guide v2

--- settings.php
+++ settings.php
@@ -9,15 +9,18 @@ include 'layout/header.php'; ?>
             <div class="col-3">
                 <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                     <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">User profile</a>
-                    <?php if ($_SESSION["role"]==1){ ?>
-                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Add new user</a>
+                    <?php if ($_SESSION["role"] == 1) { ?>
+                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Add new user</a>
                     <?php } ?>
+                    <a class="nav-link" id="v-pills-user-tab" data-toggle="pill" href="#v-pills-user" role="tab" aria-controls="v-pills-user" aria-selected="false">Add new user</a>
+
+
                 </div>
             </div>
             <div class="col-9">
                 <div class="tab-content" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                    <h4>User Profile</h4>
+                        <h4>User Profile</h4>
                         <?php include 'php/database.php';
                         $query = "SELECT * FROM user where id=" . $_SESSION["userid"];
                         $statement = $connect->prepare($query);
@@ -35,20 +38,23 @@ include 'layout/header.php'; ?>
                                         </div>
                                     </div>
                                     <div class="col-md-7">
+                                        
 
-
+                                    
                                         <div class="form-group">
-                                            <label class="col-sm-2 col-sm-2 col-form-label">Name</label>
-                                            <div class="col-sm-10">
-                                                <input type="text" class="form-control fcc"  name="user_name" value="<?php echo $row["name"] ?>">
-                                            </div>
+                                                <label class="col-sm-2 col-sm-2 col-form-label">Name</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control fcc" name="user_name" value="<?php echo $row["name"] ?>">
+                                                </div>
                                         </div>
+                                        
                                         <div class="form-group">
                                             <label class="col-sm-2 col-sm-2 col-form-label">Email</label>
                                             <div class="col-sm-10">
                                                 <input type="email" class="form-control" name="email" value="<?php echo $row["email"] ?>" name="email">
                                             </div>
                                         </div>
+
                                         <div class="form-group">
                                             <label class="col-sm-2 col-sm-2 control-label">Password</label>
                                             <div class="col-sm-10">
@@ -57,79 +63,138 @@ include 'layout/header.php'; ?>
                                         </div>
 
                                         <button type="submit" style="    margin-left: 15px;" class="btn btn-theme">Update</button>
+                                    </div>
+                                </div>
 
                             </form>
                         <?php } ?>
                     </div>
+
+                    <?php if ($_SESSION["role"] == 1) { ?>
+                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                            <h4>Add New User</h4>
+                            <br>
+                            <form class="form-horizontal style-form" action="php/adduser.php" method="post" enctype="multipart/form-data">
+                                <div class="row ">
+
+                                    <div class="col-md-12">
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-sm-2 col-form-label">Name</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control fcc" name="auser_name">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="aemail">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-sm-2 control-label">Password</label>
+                                            <div class="col-sm-10">
+                                                <input type="password" class="form-control" name="apassword">
+                                            </div>
+                                        </div>
+
+                                        <button type="submit" class="btn btn-theme">Add</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <hr>
+                            <table class="table table-striped table-advance table-hover">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 40%"> Name</th>
+                                        <th style="width: 40%"> Email</th>
+                                        <th> Remove</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php include 'php/database.php';
+                                        $query = "SELECT * FROM user";
+                                        $statement = $connect->prepare($query);
+                                        $statement->execute();
+                                        $result = $statement->fetchAll();
+                                        foreach ($result as $row) { ?>
+                                        <tr>
+                                            <td><?php echo $row["name"] ?></td>
+                                            <td><?php echo $row["email"] ?></td>
+                                            <td><a href="php/removeuser.php?id=<?php echo $row["id"] ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></a></td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+
+
+
+                        </div>
+                    <?php } ?>
+                    <div class="tab-pane fade" id="v-pills-user" role="tabpanel" aria-labelledby="v-pills-user-tab">
+                        <h4>Add New User</h4>
+                        <br>
+                        <form class="form-horizontal style-form" action="php/adduser.php" method="post" enctype="multipart/form-data">
+                            <div class="row ">
+
+                                <div class="col-md-12">
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-sm-2 col-form-label">Name</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control fcc" name="auser_name">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-sm-2 col-form-label">Email</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" name="aemail">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-sm-2 control-label">Password</label>
+                                        <div class="col-sm-10">
+                                            <input type="password" class="form-control" name="apassword">
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-theme">Add</button>
+                                </div>
+                            </div>
+                            <!-- #region -->
+                        </form>
+                        <hr>
+                        <table class="table table-striped table-advance table-hover">
+                            <thead>
+                                <tr>
+                                    <th style="width: 40%"> Name</th>
+                                    <th style="width: 40%"> Email</th>
+                                    <th> Remove</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php include 'php/database.php';
+                                $query = "SELECT * FROM user";
+                                $statement = $connect->prepare($query);
+                                $statement->execute();
+                                $result = $statement->fetchAll();
+                                foreach ($result as $row) { ?>
+                                    <tr>
+                                        <td><?php echo $row["name"] ?></td>
+                                        <td><?php echo $row["email"] ?></td>
+                                        <td><a href="php/removeuser.php?id=<?php echo $row["id"] ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></a></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+
+
+                    </div>
                 </div>
             </div>
-            <?php if ($_SESSION["role"]==1){ ?>
-            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                <h4>Add New User</h4>
-                <br>
-                <form class="form-horizontal style-form" action="php/adduser.php" method="post" enctype="multipart/form-data">
-                    <div class="row ">
-
-                        <div class="col-md-12">
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-sm-2 col-form-label">Name</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control fcc" name="auser_name">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-sm-2 col-form-label">Email</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="aemail">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-sm-2 control-label">Password</label>
-                                <div class="col-sm-10">
-                                    <input type="password" class="form-control" name="apassword">
-                                </div>
-                            </div>
-
-                            <button type="submit" class="btn btn-theme">Add</button>
-                </form>
-                <hr>
-                <table class="table table-striped table-advance table-hover">
-                <thead>
-                    <tr>
-                        <th style="width: 40%"> Name</th>
-                        <th style="width: 40%"> Email</th>
-                        <th> Remove</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php include 'php/database.php';
-                    $query = "SELECT * FROM user";
-                    $statement = $connect->prepare($query);
-                    $statement->execute();
-                    $result = $statement->fetchAll();
-                    foreach ($result as $row) { ?>
-                            <tr>
-                                <td><?php echo $row["name"] ?></td>
-                                <td><?php echo $row["email"] ?></td>
-                                <td><a href="php/removeuser.php?id=<?php echo $row["id"] ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></a></td>
-                            </tr>
-                            <?php } ?>
-                </tbody>
-                </table>
-
-            
-            </div>
-
-        </div>
-        </div>
-                    <?php } ?>
-        </div>
 
 
-        <div id="listt">
 
-        </div>
 
 
 
--- userguide.php
+++ userguide.php
@@ -20,17 +20,19 @@
 
   <header class="bg-primary text-white">
     <div class="container text-center">
-      <h1>Welcome to the user guide</h1>
-      <p class="lead">A user guide for DGNM's "Event Calendar Application" </p>
+      <h1 style="padding-top: 4%">Welcome to the user guide</h1>
+      
+      <p class="lead" style="padding-bottom: 2%">A user guide for DGNM's "Event Calendar Application" </p>
     </div>
   </header>
 
   <section id="about">
-    <div class="container">
+    <div class="col-12" style="padding-bottom: 2%">
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Calendar</h2>
-          <p class="lead">This is the <b>Dashboard</b> of this Application. Here we have included every steps of creating events, customizing, shifting and multiple views of event on calendar views.</p>
+          <p class="lead">This is the <b>Dashboard</b> of the Application. Here we have included every steps of creating events, customizing, shifting and multiple views of event on calendar views.</p>
+          <p><i>Please expend the accordions by clicking on the topics</i></p>
           <div class="container">
             <div id="accordion">
               <div class="card">
@@ -46,15 +48,15 @@
                   <div class="card-body">
                     <div class="row">
                       <img src="img\Dashboard.Calendar.png" class="img-fluid" alt="Responsive image">
-                      <p style="padding-top: 2%"><b>1. Calendar view:</b> Without any events.</p>
+                      <p style="padding-top: 2%"><b>1. Calendar view:</b> Without any events. Click on any date to create an <b>event.</b></p>
                     </div>
                     <div class="row">
                       <img src="img\cal.create.png" class="img-fluid" alt="Responsive image">
-                      <p style="padding-top: 2%"><b>2. Create Events:</b> Create events from Dashboard.</p>
+                      <p style="padding-top: 2%"><b>2. Create Events:</b> Create events from Dashboard. You must have to fill the (<b>*</b>) marked filed to proceed further.</p>
                     </div>
                     <div class="row">
                         <img src="img\cal.event.png" class="img-fluid" alt="Responsive image">
-                        <p style="padding-top: 2%"><b>3. Insert Events Information:</b> Insert event Information including - event name, date, venu, funded by, etc. For next attempt, Please click <i><b>"Choose Participant"</b></i> button to add Participant (Instructors/Resource person). </p>
+                        <p style="padding-top: 2%"><b>3. Insert Events Information:</b> Insert event information including - event name, date, venu, funded by, etc. For next attempt, Please click <i><b>"Choose Participant"</b></i> button to add Participant (Instructors/Resource person) or click <b><i>"Save"</i></b> button to save event's information without choosing participant. </p>
                       </div>
                   </div>
                 </div>
@@ -93,11 +95,12 @@
   </section>
 
   <section id="services" class="bg-light">
-    <div class="container">
+    <div class="col-12" style="padding-bottom: 2%">
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Events</h2>
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+          <p class="lead">This is the <b>Evnets</b> page of the application. Here we have included every steps of creating events, edit, delete and search.</p>
+          <p><i>Please expend the accordions by clicking on the topics</i></p>
           <div class="container">
               <div class="card">
                   <div class="card-header" id="headingThree">
@@ -110,8 +113,8 @@
                   <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                     <div class="card-body">
                         <div class="row">
-                            <img src="img\cal.eventsList.png" class="img-fluid" alt="Responsive image">
-                            <p style="padding-top: 2%"><b>1. Search any Events:</b> Here you can search any events by its name. You also can add new <b>Events</b> by clicking <i><b>"Add new Event"</b></i> button from top-left corner of the page.</p>
+                            <img src="img\page.event.png" class="img-fluid" alt="Responsive image">
+                            <p style="padding-top: 2%"><b>1. Search any Events:</b> Here you can search any events by its name. You also can add new <b>Events</b> by clicking <i><b>"Add New Event"</b></i> button from top-left corner of the page. </p>
                         </div>
                     </div>
                   </div>
@@ -142,8 +145,8 @@
     </div>
   </section>
 
-  <section id="contact">
-    <div class="container">
+  <section id="contact" style="padding-bottom: 2%">
+    <div class="col-12">
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Contact us</h2>
@@ -193,7 +196,7 @@
   </section>
 
   <section id="contact">
-    <div class="container">
+    <div class="col-12" style="padding-bottom: 2%">
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Contact us</h2>
@@ -204,7 +207,7 @@
   </section>
 
   <section id="contact">
-    <div class="container">
+    <div class="col-12" style="padding-bottom: 2%">
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Contact us</h2>
